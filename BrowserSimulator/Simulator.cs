d78500f232594e6e397fc250999b5c04005ddf19
Quoc Hung Nguyen <quochungnguyen1105@gmail.com>
2023-11-13 09:45:00 +0100

    implement undo and redo

--- BrowserSimulator/Simulator.cs
+++ BrowserSimulator/Simulator.cs
@@ -8,24 +8,84 @@ namespace BrowserSimulator
 {
     internal class Simulator
     {
+        //membervariable
+        private StringStack historyStack = new StringStack();
+        private StringStack forwardStack = new StringStack();
+        private string currentUrl = "";
+        
         public void Run()
         {
             while (true)
             {
+                Console.Clear();
+                DisplayUrl();
                 Console.Write("Please put an URL in:");
                 string url = Console.ReadLine();
-                if (url == "e")
+                switch(url)
                 {
-                    break;
-                }
-                BrowseURL(url);
+                    case "e":
+                        Environment.Exit(0);
+                        break;
+                    
+                    case "u":
+                        Undo();
+                        break;
+
+                    case "f":
+                        Redo();
+                        break;
+
+                    default:
+                        BrowseURL(url);
+                        break;
+                }    
+                
             }
         }
 
-        public void BrowseURL(string url)
+        private void BrowseURL(string url)
         {
+            if (!string.IsNullOrEmpty(currentUrl))
+            
+            {
+                historyStack.Push(currentUrl);
+                forwardStack.Clear();
+            }
+            currentUrl = url;
             Console.WriteLine($"Loading... {url}");
 
+        }
+
+        private void Undo()
+        {
+            if (historyStack.Count > 0)
+            {
+                forwardStack.Push(currentUrl);
+                currentUrl = historyStack.Pop();
+            } else
+            {
+                Console.WriteLine("Diese Funktion ist momentan nicht möglich");
+            }
+
+        }
+
+        private void Redo()
+        {
+            if (forwardStack.Count > 0)
+            {
+                historyStack.Push(currentUrl);
+                currentUrl = forwardStack.Pop();
+            }
+            else
+            {
+                Console.WriteLine("Diese Funktion ist momentan nicht möglich");
+            }
+
+        }
+
+        private void DisplayUrl()
+        {
+            Console.WriteLine($"Die Aktuelle URL ist {currentUrl}");
         }
     }
 }
